371ff02b9fe2c33de67719a0a6c74d87e22ea38d
Alex Barr <ajb7463@psu.edu>
2020-09-05 02:30:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,90 @@
+
+letter1 = input("Enter your course 1 letter grade: ")
+credit1 = input("Enter your course 1 credit: ")
+if letter1 == "A":
+  grade1 = 4.0
+  print("Grade point for course 1 is: 4.0")
+elif letter1 == "A-":
+  grade1 = 3.67
+  print("Grade point for course 1 is: 3.67")
+elif letter1 == "B+":
+  grade1 = 3.33
+  print("Grade point for course 1 is: 3.33")
+elif letter1 == "B":
+  grade1 = 3.0
+  print("Grade point for course 1 is: 3.0")
+elif letter1 == "B-":
+  grade1 = 2.67
+  print("Grade point for course 1 is: 2.67")
+elif letter1 == "C+":
+  grade1 = 2.33
+  print("Grade point for course 1 is: 2.33")
+elif letter1 == "C":
+  grade1 = 2.0
+  print("Grade point for course 1 is: 2.0")
+elif letter1 == "D":
+  grade1 = 1.0
+  print("Grade point for course 1 is: 1.0")
+else:
+  grade1 = 0
+  print("Grade point for course 1 is: 0.0")
+letter2 = input("Enter your course 2 letter grade: ")
+credit2 = input("Enter your course 2 credit: ")
+if letter2 == "A":
+  grade2 = 4.0
+  print("Grade point for course 2 is: 4.0")
+elif letter2 == "A-":
+  grade2 = 3.67
+  print("Grade point for course 2 is: 3.67")
+elif letter2 == "B+":
+  grade2 = 3.33
+  print("Grade point for course 2 is: 3.33")
+elif letter2 == "B":
+  grade2 = 3.0
+  print("Grade point for course 2 is: 3.0")
+elif letter2 == "B-":
+  grade2 = 2.67
+  print("Grade point for course 2 is: 2.67")
+elif letter2 == "C+":
+  grade2 = 2.33
+  print("Grade point for course 2 is: 2.33")
+elif letter2 == "C":
+  grade2 = 2.0
+  print("Grade point for course 2 is: 2.0")
+elif letter2 == "D":
+  grade2 = 1.0
+  print("Grade point for course 2 is: 1.0")
+else:
+  grade2 = 0
+  print("Grade point for course 2 is: 0.0")
+letter3 = input("Enter your course 3 letter grade: ")
+credit3 = input("Enter your course 3 credit: ")
+if letter3 == "A":
+  grade3 = 4.0
+  print("Grade point for course 3 is: 4.0")
+elif letter3 == "A-":
+  grade3 = 3.67
+  print("Grade point for course 3 is: 3.67")
+elif letter3 == "B+":
+  grade3 = 3.33
+  print("Grade point for course 3 is: 3.33")
+elif letter3 == "B":
+  grade3 = 3.0
+  print("Grade point for course 3 is: 3.0")
+elif letter3 == "B-":
+  grade1 = 2.67
+  print("Grade point for course 3 is: 2.67")
+elif letter3 == "C+":
+  grade3 = 2.33
+  print("Grade point for course 3 is: 2.33")
+elif letter3 == "C":
+  grade3 = 2.0
+  print("Grade point for course 3 is: 2.0")
+elif letter3 == "D":
+  grade3 = 1.0
+  print("Grade point for course 3 is: 1.0")
+else:
+  grade3 = 0
+  print("Grade point for course 3 is: 0.0")
+finalgrade = (grade1 * credit1 + grade2 * credit2 + grade3 * credit3) / (credit1 + credit2 + credit3)
+input("Your GPA is: {finalgrade}")
